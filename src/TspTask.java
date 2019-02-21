cca476cf5824967da369b1c81f0e21f8969c054d
kostmetallist <kostmetallist@gmail.com>
2019-02-21 19:41:56 +0300

    Created TspTask class and a Voyage class extending Organism

---
+++ src/TspTask.java
@@ -0,0 +1,13 @@
+// neighbout representation of towns in list
+class Voyage extends Organism {
+
+    public Voyage(byte[] neighList) {
+        super.(neighList);
+    }
+}
+
+public class TspTask {
+    public static void main(String[] args) {
+
+    }
+}
